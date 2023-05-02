787a56014c9f6c1d6a904e719296742d62f5ed63
Moaz <moazomran20@gmail.com>
2023-05-03 00:20:20 +0200

    radio ui finished

--- lib/tabs/radio.dart
+++ lib/tabs/radio.dart
@@ -1,9 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class RadioBar extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+            child: Container(
+                alignment: Alignment.center,
+                height: 220,
+                child: Image.asset(
+                  "assets/images/radio_background.png",
+                  fit: BoxFit.fill,
+                )),
+          ),
+          Text(
+            "اذاعه القرآن الكريم",
+            style: GoogleFonts.amiri(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: ImageIcon(
+                  AssetImage(
+                    "assets/images/Icon metro-next.png",
+                  ),
+                  color: Theme.of(context).primaryColor,
+                ),
+
+              ),
+              SizedBox(width: 20,),
+              IconButton(
+                onPressed: () {},
+                icon: ImageIcon(
+                  AssetImage(
+                    "assets/images/Icon awesome-play.png",
+                  ),
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              SizedBox(width: 20,),
+              IconButton(
+                onPressed: () {},
+                icon: ImageIcon(
+                  AssetImage(
+                    "assets/images/icon_next.png",
+                  ),
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
